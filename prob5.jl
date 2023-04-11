2c58d7953e0ffa2ba670d9ac29661102a8495539
Yuji Seki <ysrichard1123@gmail.com>
2023-04-11 22:38:28 +0900

    [Add] hamiltonian

--- prob5.jl
+++ prob5.jl
@@ -1,4 +1,27 @@
 t = 1 
 U = 2 
 mix_ratio = 0.2 
-delta = 0.1 
+delta_old = 0.1 
+
+H_up = [
+        0.0　0.0
+        0.0　0.0
+        ]
+
+H_dn = [
+        0.0　0.0
+        0.0　0.0
+        ]
+
+for i = 1:10000
+    H_up[1,1] = 0.5*(1-delta_old)*U
+    H_up[2,2] = 0.5*(1+delta_old)*U
+    H_up[1,2] = -t
+    H_up[2,1] = -t
+
+    H_dn[1,1] = 0.5*(1+delta_old)*U
+    H_dn[2,2] = 0.5*(1-delta_old)*U
+    H_dn[1,2] = -t
+    H_dn[2,1] = -t
+
+end 
